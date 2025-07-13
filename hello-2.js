7678dfe3f7e48271bd3022445581f8f288920a13
Saodat <saodat.riskiyeva@gmail.com>
2025-07-13 11:32:42 -0400

    practising module.exports

---
+++ hello-2.js
@@ -0,0 +1,7 @@
+var str1 = "Hello 2";
+const str2 = "World II";
+module.exports = {
+  first: str1,
+  second: str2,
+  date: Date(),
+};
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const c = require("./hello.js");
+const d = require("./hello-2.js");
+
 const a = 100;
 const b = "World";
-console.log(a + b);
+// console.log(a + b);
+console.log(c);
+console.log(d);
