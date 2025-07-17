e1f697a545c1e3a3ad592e26ec94ad33ef1e0987
Saodat <saodat.riskiyeva@gmail.com>
2025-07-16 22:20:19 -0400

    working with app middleware

--- index.js
+++ index.js
@@ -22,13 +22,16 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  console.log("I am in the middle");
+});
 
 app.get("/", function (req, res) {
   res.send("GET sent");
 });
 
 app.post("/", function (req, res) {
-  console.log(req.body);
+  // console.log(req.body);
   res.send("POST sent");
 });
 
