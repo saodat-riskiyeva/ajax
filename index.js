d09a87d49ab09d80c3c93fe7bbc30a6063493c06
Saodat <saodat.riskiyeva@gmail.com>
2025-07-16 22:38:45 -0400

    login check setup

--- index.js
+++ index.js
@@ -19,19 +19,26 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const data = { user: "admin", password: "pass" };
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-  console.log("I am in the middle");
-});
+// app.use(function (req, res, next) {
+//   console.log("I am in the middle");
+// });
 
 app.get("/", function (req, res) {
-  res.send("GET sent");
+  res.send("GET sent 2");
 });
 
-app.post("/", function (req, res) {
-  // console.log(req.body);
+app.post("/login", function (req, res) {
+  if (data.user == req.body.user && data.password == req.body.password) {
+    res.write("Success");
+  } else {
+    res.write("Failure");
+  }
+
+  console.log(req.body.user);
   res.send("POST sent");
 });
 
