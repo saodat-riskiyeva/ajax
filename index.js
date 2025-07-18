a3531beae4ed6872590acbc2ab905a2681b6587e
Saodat <saodat.riskiyeva@gmail.com>
2025-07-18 11:12:57 -0400

    get response data

--- index.js
+++ index.js
@@ -16,37 +16,39 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
 app.post("/users", function (req, res) {
-  //create and add user
   req.body.id = data.users.length + 1;
   data.users.push(req.body);
-  console.log(data);
-  console.log(req.body);
 
   res.send("CREATE user by ID");
 });
 
 app.get("/users", function (req, res) {
-  // get data for all users
-  res.send("GET all users data");
+  res.json(data);
 });
 
-app.get("/users/:id", function (req, res) {
-  console.log(req.params);
-  // get user data by id
-  res.send("GET user data by ID");
+  res.send(getRow(req.params.id));
 });
 
 app.put("/users/:id", function (req, res) {
-  // update user data by id
   res.send("UPDATE user data by ID");
 });
 
 app.delete("/users/:id", function (req, res) {
-  // delete user by id
   res.send("DELETE user by ID");
 });
 
 app.listen(3000);
+
+function getRow(id) {
+  for (let item of data.users) {
+    console.log(typeof item.id);
+    console.log(typeof id);
+    if (item.id == Number(id)) {
+      return item;
+    }
+  }
+  return false;
+}
 
 // const http = require("http");
 // const fs = require("fs");
